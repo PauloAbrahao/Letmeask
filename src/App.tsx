9f9f4f24d213a8a14290328ea4e150dd324d4067
PauloAbrahao <ab.cod@outlook.com>
2023-11-12 14:53:42 -0300

    refactor: back to original path file

--- src/App.tsx
+++ src/App.tsx
@@ -13,10 +13,10 @@ function App() {
     <BrowserRouter>
       <AuthContextProvider>
         <Switch>
-          <Route path="./" exact component={Home} />
-          <Route path="./rooms/new" component={NewRoom} />
-          <Route path="./rooms/:id" component={Room} />
-          <Route path="./admin/rooms/:id" component={AdminRoom} />
+          <Route path="/" exact component={Home} />
+          <Route path="/rooms/new" component={NewRoom} />
+          <Route path="/rooms/:id" component={Room} />
+          <Route path="/admin/rooms/:id" component={AdminRoom} />
         </Switch>
       </AuthContextProvider>
     </BrowserRouter>
